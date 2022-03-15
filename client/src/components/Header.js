b404d4e4ff5bdd0785925637a41f559e0f063a51
Steven Trujillo <potencyalmighty@outlook.com>
2022-03-15 10:28:04 -0600

    Tinkered with font awesome React libraries to find a hamburger bar icon

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
     const[click, setClick] = useState(false)
   return (
     <div className='header'>
         <Link to="/" className="logo"><h1>My Page</h1></Link>
+        <FontAwesomeIcon icon={faBars} />
         <ul className='nav-text'>
             <Link to="/" className='home'><li>Home</li></Link>
             <li className='sign'>Sign Up</li>
