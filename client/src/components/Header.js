f23add606654bc5ee821107288f377269b2db1ef
Steven Trujillo <potencyalmighty@outlook.com>
2022-03-15 10:45:37 -0600

    Made the icons from fontawesom switch between faBars and faTimes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
-    const[click, setClick] = useState(false)
+    const[click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
   return (
     <div className='header'>
         <Link to="/" className="logo"><h1>My Page</h1></Link>
-        <FontAwesomeIcon icon={faBars} />
+        <div className='menu-icon' onClick={handleClick}>
+            <FontAwesomeIcon icon={click ? faTimes : faBars} />
+        </div>
+        
         <ul className='nav-text'>
             <Link to="/" className='home'><li>Home</li></Link>
             <li className='sign'>Sign Up</li>
