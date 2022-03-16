896ac71de5267aa37e1b0c8cb62245ec27619d7b
Steven Trujillo <potencyalmighty@outlook.com>
2022-03-15 21:47:46 -0600

    Made seperate css folders for Button and Header components, and tinkered with the header's styling to make it more responsive

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,24 +2,41 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
+import { Button } from './Button'
+import './Header.css';
 
 export default function Header() {
     const[click, setClick] = useState(false);
+    const[button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } else {
+            setButton(true)
+        }
+    }
+
+    window.addEventListener('resize', showButton);
 
   return (
-    <div className='header'>
-        <Link to="/" className="logo"><h1>My Page</h1></Link>
-        <div className='menu-icon' onClick={handleClick}>
-            <FontAwesomeIcon icon={click ? faTimes : faBars} />
+    <nav className='header'>
+        <div className='nav-container'>
+            <Link to="/" className="logo"><h1>My Page</h1></Link>
+            <div className='menu-icon' onClick={handleClick}>
+                <FontAwesomeIcon icon={click ? faTimes : faBars} />
+            </div>
+            <ul className={click ? 'nav-text active' : 'nav-text'}>
+                <li className='nav-item'><Link to="/" className='nav-links' onClick={closeMobileMenu}>Home</Link></li>
+                <li className='nav-item'><Link to="/contact" className='nav-links' onClick={closeMobileMenu}>Contact</Link></li>
+                <li className='nav-item'><Link to="/sign-up" className='nav-links-mobile' onClick={closeMobileMenu}>Sign Up</Link></li>
+            </ul>
+            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
         
-        <ul className='nav-text'>
-            <Link to="/" className='home'><li>Home</li></Link>
-            <li className='sign'>Sign Up</li>
-            <li>Contact</li>
-        </ul>
-    </div>
+    </nav>
   )
 }
